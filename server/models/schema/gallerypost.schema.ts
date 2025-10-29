de41f36bb0484781160b7bbf97f693ae6fc26383
Erika Ding <erikajding@gmail.com>
2025-10-28 21:04:31 -0400

    Started gallery post database schema and backend implementation

---
+++ server/models/schema/gallerypost.schema.ts
@@ -0,0 +1,35 @@
+import { Schema } from 'mongoose';
+
+/**
+ * Mongoose schema for the Gallery collection.
+ *
+ * This schema defines the structure of a gallery post in the database.
+ * Each gallery post includes the following fields: (tentative)
+ * - title
+ * - description
+ * - author (by username)
+ * - model (by filepath, referencing the Media Schema type)
+ * - post date and time
+ */
+const galleryPostSchema: Schema = new Schema(
+  {
+    title: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    author: {
+      type: String,
+    },
+    model: {
+      type: String,
+    },
+    postDateTime: {
+      type: Date,
+    }
+  },
+  { collection: 'GalleryPost' },
+)
+
+export default galleryPostSchema;
---
+++ server/services/gallerypost.service.ts
@@ -0,0 +1,47 @@
+import { DatabaseGalleryPost, GalleryPost, GalleryPostResponse } from "@fake-stack-overflow/shared";
+import GalleryPostModel from "../models/gallerypost.model";
+
+export const getAllGalleryPosts = async (): Promise<DatabaseGalleryPost[] | { error: string }> => {
+  try {
+    const posts = await GalleryPostModel.find({});
+    return posts;
+  } catch (err) {
+    return { error: (err as Error).message };
+  }
+}
+
+export const createGalleryPost = async (galleryPost: GalleryPost): Promise<GalleryPostResponse> => {
+  try {
+    const newGalleryPost = await GalleryPostModel.create(galleryPost);
+
+    if (!newGalleryPost) {
+      throw new Error('Failed to create gallery post');
+    }
+
+    return newGalleryPost;
+  } catch (error) {
+    return { error: (error as Error).message };
+  }
+}
+
+/**
+ * Retrieves a gallery post by id.
+ * 
+ * @param id 
+ * @returns 
+ */
+export const getGalleryPostById = async (
+  id: string,
+): Promise<GalleryPostResponse> => {
+  try {
+    const galleryPost = await GalleryPostModel.findById(id);
+
+    if (!galleryPost) {
+      throw new Error('Failed to get gallery post');
+    }
+
+    return galleryPost;
+  } catch (error) {
+    return { error: (error as Error).message };
+  }
+}
