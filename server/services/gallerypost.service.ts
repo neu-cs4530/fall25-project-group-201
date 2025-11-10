0525a53ab2424e7acaf66d76e424f1ef0872259b
nMDaas <63910563+nMDaas@users.noreply.github.com>
2025-11-09 22:18:00 -0500

    Deleting gallery post deletes associated media as well

--- server/services/gallerypost.service.ts
+++ server/services/gallerypost.service.ts
@@ -1,5 +1,7 @@
 import { DatabaseGalleryPost, GalleryPost, GalleryPostResponse } from '@fake-stack-overflow/shared';
 import GalleryPostModel from '../models/gallerypost.model';
+import { promises as fs } from 'fs';
+import path from 'path';
 
 export const getAllGalleryPosts = async (): Promise<DatabaseGalleryPost[] | { error: string }> => {
   try {
@@ -44,15 +46,42 @@ export const getGalleryPostById = async (id: string): Promise<GalleryPostRespons
   }
 };
 
-export const deleteGalleryPost = async (id: string, username: string): Promise<GalleryPostResponse> => {
+export const deleteGalleryPost = async (
+  id: string,
+  username: string
+): Promise<GalleryPostResponse> => {
   try {
+    // Find the gallery post
+    const galleryPost = await GalleryPostModel.findOne({
+      _id: id,
+      username: username,
+    });
+
+    if (!galleryPost) {
+      throw new Error('Gallery post not found');
+    }
+
+    // Get full filepath
+    const projectRoot = path.resolve(__dirname, '../../');
+    const filePath = path.join(projectRoot, 'client', 'public', galleryPost.media);
+
+    // Delete media
+    if (galleryPost.media) {
+      try {
+        await fs.unlink(filePath);
+      } catch (err) {
+        throw new Error(`Failed to delete media: ${filePath}`);
+      }
+    }
+
+    // Delete gallery post
     const deletedGalleryPost = await GalleryPostModel.findOneAndDelete({
       _id: id,
       username: username,
     });
 
     if (!deletedGalleryPost) {
-      throw new Error('Failed to delete gallery post');
+      throw new Error('Failed to delete gallery post after deleting media');
     }
 
     return deletedGalleryPost;
