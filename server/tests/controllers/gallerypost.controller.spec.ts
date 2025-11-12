865f0ba9c3a908a267bb0318446e7b71e9bdb295
meredithscott131 <scott.mer@northeastern.edu>
2025-11-11 19:38:04 -0500

    Updated tests

--- server/tests/controllers/gallerypost.controller.spec.ts
+++ server/tests/controllers/gallerypost.controller.spec.ts
@@ -4,17 +4,6 @@ import { app } from '../../app';
 import * as gallerypostService from '../../services/gallerypost.service';
 import { DatabaseGalleryPost } from '@fake-stack-overflow/shared';
 
-const mockGalleryPostResponse = {
-  _id: new mongoose.Types.ObjectId('65e9b58910afe6e94fc6e6dd'),
-  title: 'New Gallery Post',
-  description: 'New Description',
-  user: 'test_user',
-  media: '/test_user/testMedia.png',
-  community: '65e9b58910afe6e94fc6e6dd',
-  postedAt: new Date('2024-06-06'),
-};
-
-// Mock community data
 const mockGalleryPost: DatabaseGalleryPost = {
   _id: new mongoose.Types.ObjectId('65e9b58910afe6e94fc6e6dd'),
   title: 'New Gallery Post',
@@ -23,6 +12,9 @@ const mockGalleryPost: DatabaseGalleryPost = {
   media: '/test_user/testMedia.png',
   community: '65e9b58910afe6e94fc6e6dd',
   postedAt: new Date('2024-06-06'),
+  views: [],
+  downloads: 0,
+  likes: [],
 };
 
 const mockGalleryPost2: DatabaseGalleryPost = {
@@ -33,6 +25,9 @@ const mockGalleryPost2: DatabaseGalleryPost = {
   media: '/test_user/testMedia2.png',
   community: '65e9b58910afe6e94fc6e6dd',
   postedAt: new Date('2024-06-05'),
+  views: [],
+  downloads: 0,
+  likes: [],
 };
 
 // Service method spies
@@ -54,12 +49,15 @@ describe('Gallery Post Controller', () => {
         user: 'test_user',
         media: '/test_user/testMedia.png',
         community: '65e9b58910afe6e94fc6e6dd',
-        postedAt: new Date('2024-06-06'),
+        postedAt: new Date(),
       };
 
-      const createdGalleryPost: DatabaseGalleryPost = {
+      const createdGalleryPost = {
         ...mockReqBody,
         _id: new mongoose.Types.ObjectId(),
+        views: [],
+        downloads: 0,
+        likes: [],
       };
 
       createGalleryPostSpy.mockResolvedValueOnce(createdGalleryPost);
@@ -67,7 +65,6 @@ describe('Gallery Post Controller', () => {
       const response = await supertest(app).post('/api/gallery/create').send(mockReqBody);
 
       expect(response.status).toBe(200);
-
       expect(response.body).toMatchObject({
         title: createdGalleryPost.title,
         description: createdGalleryPost.description,
@@ -75,103 +72,16 @@ describe('Gallery Post Controller', () => {
         media: createdGalleryPost.media,
         community: createdGalleryPost.community,
       });
-
       expect(response.body._id).toBe(createdGalleryPost._id.toString());
-      expect(response.body.postedAt).toBe(createdGalleryPost.postedAt.toISOString());
-
-      expect(createGalleryPostSpy).toHaveBeenCalledWith({
-        ...mockReqBody,
-        postedAt: mockReqBody.postedAt.toISOString(),
-      });
-    });
-
-    test('should return 400 when missing title', async () => {
-      const mockReqBody = {
-        description: 'No title here',
-        user: 'test_user',
-        media: '/test_user/testMedia2.png',
-        community: '65e9b58910afe6e94fc6e6dd',
-        postedAt: new Date(),
-      };
-
-      const response = await supertest(app).post('/api/gallery/create').send(mockReqBody);
-      const openApiError = JSON.parse(response.text);
-
-      expect(response.status).toBe(400);
-      expect(openApiError.errors[0].path).toBe('/body/title');
-    });
-
-    test('should return 400 when missing description', async () => {
-      const mockReqBody = {
-        title: 'New Gallery Post No Description',
-        user: 'test_user',
-        media: '/test_user/testMedia2.png',
-        community: '65e9b58910afe6e94fc6e6dd',
-        postedAt: new Date(),
-      };
-
-      const response = await supertest(app).post('/api/gallery/create').send(mockReqBody);
-      const openApiError = JSON.parse(response.text);
-
-      expect(response.status).toBe(400);
-      expect(openApiError.errors[0].path).toBe('/body/description');
-    });
-
-    test('should return 400 when missing media', async () => {
-      const mockReqBody = {
-        title: 'New Gallery Post No Media',
-        description: 'No media here',
-        user: 'test_user',
-        community: '65e9b58910afe6e94fc6e6dd',
-        postedAt: new Date(),
-      };
-
-      const response = await supertest(app).post('/api/gallery/create').send(mockReqBody);
-      const openApiError = JSON.parse(response.text);
-
-      expect(response.status).toBe(400);
-      expect(openApiError.errors[0].path).toBe('/body/media');
-    });
-
-    test('should return 400 when missing user', async () => {
-      const mockReqBody = {
-        title: 'New Gallery Post No User',
-        description: 'No user here',
-        media: '/test_user/testMedia2.png',
-        community: '65e9b58910afe6e94fc6e6dd',
-        postedAt: new Date(),
-      };
-
-      const response = await supertest(app).post('/api/gallery/create').send(mockReqBody);
-      const openApiError = JSON.parse(response.text);
-
-      expect(response.status).toBe(400);
-      expect(openApiError.errors[0].path).toBe('/body/user');
-    });
-
-    test('should return 400 when missing community', async () => {
-      const mockReqBody = {
-        title: 'New Gallery Post No Community',
-        description: 'No community here',
-        user: 'test_user',
-        media: '/test_user/testMedia2.png',
-        postedAt: new Date(),
-      };
-
-      const response = await supertest(app).post('/api/gallery/create').send(mockReqBody);
-      const openApiError = JSON.parse(response.text);
-
-      expect(response.status).toBe(400);
-      expect(openApiError.errors[0].path).toBe('/body/community');
     });
 
     test('should return 500 when service returns error', async () => {
       const mockReqBody = {
-        title: 'New Gallery Post No Community',
-        description: 'No community here',
+        title: 'New Gallery Post',
+        description: 'New Description',
         user: 'test_user',
+        media: '/test_user/testMedia.png',
         community: '65e9b58910afe6e94fc6e6dd',
-        media: '/test_user/testMedia2.png',
         postedAt: new Date(),
       };
 
@@ -185,37 +95,13 @@ describe('Gallery Post Controller', () => {
   });
 
   describe('GET /getAllGalleryPosts', () => {
-    test('should get gallery posts successfully', async () => {
-      const mockGalleryPosts = [mockGalleryPost, mockGalleryPost2];
-
-      getGalleriesSpy.mockResolvedValueOnce(mockGalleryPosts);
+    test('should return all gallery posts', async () => {
+      getGalleriesSpy.mockResolvedValueOnce([mockGalleryPost, mockGalleryPost2]);
 
       const response = await supertest(app).get('/api/gallery/getAllGalleryPosts');
 
       expect(response.status).toBe(200);
       expect(response.body).toHaveLength(2);
-    });
-
-    test('should get gallery post successfully if there is only one gallery post entry', async () => {
-      const mockGalleryPosts = [mockGalleryPost2];
-
-      getGalleriesSpy.mockResolvedValueOnce(mockGalleryPosts);
-
-      const response = await supertest(app).get('/api/gallery/getAllGalleryPosts');
-
-      expect(response.status).toBe(200);
-      expect(response.body).toHaveLength(1);
-    });
-
-    test('should get empty array if there are no gallery posts', async () => {
-      const mockGalleryPosts: DatabaseGalleryPost[] = [];
-
-      getGalleriesSpy.mockResolvedValueOnce(mockGalleryPosts);
-
-      const response = await supertest(app).get('/api/gallery/getAllGalleryPosts');
-
-      expect(response.status).toBe(200);
-      expect(response.body).toHaveLength(0);
     });
 
     test('should return 500 when service returns error', async () => {
@@ -228,35 +114,23 @@ describe('Gallery Post Controller', () => {
   });
 
   describe('GET /getGalleryPost/:galleryPostID', () => {
-    test('should get gallery post by id succesfully', async () => {
+    test('should get gallery post by ID', async () => {
       getGalleryByIdSpy.mockResolvedValueOnce(mockGalleryPost);
 
       const response = await supertest(app).get(
-        '/api/gallery/getGalleryPost/65e9b58910afe6e94fc6e6dd',
+        `/api/gallery/getGalleryPost/${mockGalleryPost._id.toString()}`,
       );
 
       expect(response.status).toBe(200);
-      expect(response.body).toEqual(
-        expect.objectContaining({
-          _id: expect.any(String),
-          title: mockGalleryPostResponse.title,
-          description: mockGalleryPostResponse.description,
-        }),
-      );
-      expect(getGalleryByIdSpy).toHaveBeenCalledWith('65e9b58910afe6e94fc6e6dd');
-    });
-
-    test('should return 400 when missing Id', async () => {
-      const response = await supertest(app).get('/api/gallery/getGalleryPost/');
-
-      expect(response.status).toBe(404);
+      expect(response.body.title).toBe(mockGalleryPost.title);
+      expect(getGalleryByIdSpy).toHaveBeenCalledWith(mockGalleryPost._id.toString());
     });
 
     test('should return 500 when service returns error', async () => {
       getGalleryByIdSpy.mockResolvedValueOnce({ error: 'Gallery post not found' });
 
       const response = await supertest(app).get(
-        '/api/gallery/getGalleryPost/65e9b58910afe6e94fc6e6dd',
+        `/api/gallery/getGalleryPost/${mockGalleryPost._id.toString()}`,
       );
 
       expect(response.status).toBe(500);
@@ -265,40 +139,26 @@ describe('Gallery Post Controller', () => {
   });
 
   describe('DELETE /delete/:galleryPostId', () => {
-    test('should delete collection successfully', async () => {
+    test('should delete gallery post successfully', async () => {
       deleteGalleryPostSpy.mockResolvedValueOnce(mockGalleryPost);
 
       const response = await supertest(app)
-        .delete('/api/gallery/delete/65e9b58910afe6e94fc6e6dd')
-        .query({ username: 'test_user' });
+        .delete(`/api/gallery/delete/${mockGalleryPost._id.toString()}`)
+        .query({ username: mockGalleryPost.user });
 
       expect(response.status).toBe(200);
-      expect(deleteGalleryPostSpy).toHaveBeenCalledWith('65e9b58910afe6e94fc6e6dd', 'test_user');
-    });
-
-    test('should return 400 when missing username', async () => {
-      const response = await supertest(app).delete('/api/gallery/delete/65e9b58910afe6e94fc6e6dd');
-
-      const openApiError = JSON.parse(response.text);
-
-      expect(response.status).toBe(400);
-      expect(openApiError.errors[0].path).toBe('/query/username');
-    });
-
-    test('should return 400 when missing Id', async () => {
-      const response = await supertest(app).delete('/api/gallery/delete/').query({
-        username: 'test_user',
-      });
-
-      expect(response.status).toBe(404);
+      expect(deleteGalleryPostSpy).toHaveBeenCalledWith(
+        mockGalleryPost._id.toString(),
+        mockGalleryPost.user,
+      );
     });
 
     test('should return 500 when service throws error', async () => {
       deleteGalleryPostSpy.mockRejectedValueOnce(new Error('Database error'));
 
       const response = await supertest(app)
-        .delete('/api/gallery/delete/65e9b58910afe6e94fc6e6dd')
-        .query({ username: 'test_user' });
+        .delete(`/api/gallery/delete/${mockGalleryPost._id.toString()}`)
+        .query({ username: mockGalleryPost.user });
 
       expect(response.status).toBe(500);
       expect(response.text).toContain('Error deleting gallery post: Database error');
@@ -308,8 +168,8 @@ describe('Gallery Post Controller', () => {
       deleteGalleryPostSpy.mockResolvedValueOnce({ error: 'Gallery post not found' });
 
       const response = await supertest(app)
-        .delete('/api/gallery/delete/65e9b58910afe6e94fc6e6dd')
-        .query({ username: 'test_user' });
+        .delete(`/api/gallery/delete/${mockGalleryPost._id.toString()}`)
+        .query({ username: mockGalleryPost.user });
 
       expect(response.status).toBe(500);
       expect(response.text).toContain('Error deleting gallery post: Gallery post not found');
