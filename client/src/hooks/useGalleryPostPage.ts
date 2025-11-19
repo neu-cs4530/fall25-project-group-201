16863e59e08d71639a9d6bc253aba132be636c63
nMDaas <63910563+nMDaas@users.noreply.github.com>
2025-11-19 09:28:58 -0500

    media and thumbnail media documents deleting if gallery post deleted

--- client/src/hooks/useGalleryPostPage.ts
+++ client/src/hooks/useGalleryPostPage.ts
@@ -7,6 +7,9 @@ import {
   toggleGalleryPostLikes,
   deleteGalleryPost,
 } from '../services/galleryService';
+import {
+  deleteMedia
+} from '../services/mediaService';
 import useUserContext from './useUserContext';
 import { DatabaseGalleryPost } from '../types/types';
 
@@ -104,6 +107,24 @@ const useGalleryPostPage = () => {
    */
   const removePost = async () => {
     if (!post) return;
+
+    // Delete media only if it is a media path not a media url (embed)
+    if (post.media.startsWith('/userData/')) {
+      try {
+        await deleteMedia(post.media);
+      } catch {
+        setError('Failed to delete media.');
+      }
+    }
+
+    if (post.thumbnailMedia) {
+      try {
+        await deleteMedia(post.thumbnailMedia);
+      } catch {
+        setError('Failed to delete thumbanail media.');
+      }
+    }
+
     try {
       await deleteGalleryPost(post._id.toString(), user.username);
       navigate(`/communities/${post.community}`);
