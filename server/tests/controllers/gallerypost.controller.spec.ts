b5937757ab416e488bf24daa8b8cf9512b102d4e
nMDaas <63910563+nMDaas@users.noreply.github.com>
2025-11-19 19:42:48 -0500

    Updated gallery post tests

--- server/tests/controllers/gallerypost.controller.spec.ts
+++ server/tests/controllers/gallerypost.controller.spec.ts
@@ -15,6 +15,7 @@ const mockGalleryPost: DatabaseGalleryPost = {
   views: 0,
   downloads: 0,
   likes: [],
+  mediaSize: '13 GB',
 };
 
 const mockGalleryPost2: DatabaseGalleryPost = {
@@ -28,6 +29,7 @@ const mockGalleryPost2: DatabaseGalleryPost = {
   views: 0,
   downloads: 0,
   likes: [],
+  mediaSize: '14 GB',
 };
 
 const createGalleryPostSpy = jest.spyOn(gallerypostService, 'createGalleryPost');
@@ -53,6 +55,7 @@ describe('Gallery Post Controller', () => {
         media: '/test_user/testMedia.png',
         community: '65e9b58910afe6e94fc6e6dd',
         postedAt: new Date('2024-06-06'),
+        mediaSize: '13 GB',
       };
 
       const created = {
