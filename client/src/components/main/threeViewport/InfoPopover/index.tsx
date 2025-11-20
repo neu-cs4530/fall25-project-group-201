8ac3c15edb08504767dde338d94996264256cfcb
Erika Ding <erikajding@gmail.com>
2025-11-19 23:46:30 -0500

    Preliminary info button on viewport

---
+++ client/src/components/main/threeViewport/InfoPopover/index.tsx
@@ -0,0 +1,25 @@
+import './index.css';
+import useInfoPopover from '../../../../hooks/useInfoPopover';
+
+const InfoPopover = () => {
+  const {buttonRef, popoverRef, isVisible, toggleVisibility} = useInfoPopover();
+
+  return (
+    <div
+      id="popover-content"
+      ref={popoverRef}
+      className="popover-content"
+      role="dialog"
+      aria-modal="true"
+    >
+      Welcome to the 3D viewport!
+      <br/>
+      <p>Click and drag to turn, and scroll to zoom in and out.</p>
+      <p>Buttons in the top left are the reset camera button, </p>
+      <p>perspective/orthographic mode button, and </p>
+      <p>some toggles between HDRI lighting setups.</p>
+    </div>
+  );
+};
+
+export default InfoPopover;
---
+++ client/src/hooks/useInfoPopover.ts
@@ -0,0 +1,36 @@
+import { useState, useRef, useEffect } from "react";
+
+const useInfoPopover = () => {
+  const [isVisible, setIsVisible] = useState(false); // Manages the visibility state of the popover
+  const popoverRef = useRef<HTMLDivElement>(null); // Reference to the popover element
+  const buttonRef = useRef<HTMLButtonElement>(null); // Reference to the button element that triggers the popover
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        popoverRef.current && 
+        buttonRef.current &&
+        !popoverRef.current.contains(event.target as Node) &&
+        !buttonRef.current.contains(event.target as Node)
+      ) {
+        setIsVisible(false); // Close the popover if clicked outside
+      }
+    };
+
+    if (isVisible) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+  }, [isVisible]);
+
+  return {buttonRef, popoverRef, isVisible, toggleVisibility}
+};
+
+export default useInfoPopover;
