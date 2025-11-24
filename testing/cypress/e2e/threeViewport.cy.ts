2a2b21abf584269e5b707272db542261331871eb
nMDaas <63910563+nMDaas@users.noreply.github.com>
2025-11-24 16:57:41 -0500

    rotation, panning, tilting, zooming

--- testing/cypress/e2e/threeViewport.cy.ts
+++ testing/cypress/e2e/threeViewport.cy.ts
@@ -14,7 +14,7 @@ describe('Cypress tests for Three Viewport controls', function () {
     });
 
     // Variables for test
-    const testUser = 'user234'
+    const testUser = 'user345'
 
     it('Three Viewport supports rotation of model', function () {
         goToCommunities();
@@ -39,6 +39,7 @@ describe('Cypress tests for Three Viewport controls', function () {
         cy.get('.viewport-canvas canvas')
             .should('exist');
 
+        // Rotation
         cy.get('.viewport-canvas canvas')
             .trigger('mousedown', { clientX: 100, clientY: 100, button: 0 })
             .trigger('mousemove', { clientX: 200, clientY: 150, button: 0 })
@@ -50,7 +51,45 @@ describe('Cypress tests for Three Viewport controls', function () {
             .trigger('mousemove', { clientX: 5, clientY: 400, button: 0 })
             .trigger('mousemove', { clientX: 100, clientY: 200, button: 0 })
             .trigger('mouseup', { force: true });
+
+        // Panning 
+        cy.get('.viewport-canvas').trigger('keydown', { key: 'ArrowUp' });
+        cy.wait(150);
+        cy.get('.viewport-canvas').trigger('keyup', { key: 'ArrowUp' });
+        cy.get('.viewport-canvas').trigger('keydown', { key: 'ArrowDown' });
+        cy.wait(200);
+        cy.get('.viewport-canvas').trigger('keyup', { key: 'ArrowDown' });
+        cy.get('.viewport-canvas').trigger('keydown', { key: 'ArrowLeft' });
+        cy.wait(200);
+        cy.get('.viewport-canvas').trigger('keyup', { key: 'ArrowLeft' });
+        cy.get('.viewport-canvas').trigger('keydown', { key: 'ArrowRight' });
+        cy.wait(200);
+        cy.get('.viewport-canvas').trigger('keyup', { key: 'ArrowRight' });
+
+        // Tilting 
+        cy.get('.viewport-canvas').trigger('keydown', { key: 'w' });
+        cy.wait(200);
+        cy.get('.viewport-canvas').trigger('keyup', { key: 'w' });
+        cy.get('.viewport-canvas').trigger('keydown', { key: 'a' });
+        cy.wait(200);
+        cy.get('.viewport-canvas').trigger('keyup', { key: 'a' });
+        cy.get('.viewport-canvas').trigger('keydown', { key: 's' });
+        cy.wait(200);
+        cy.get('.viewport-canvas').trigger('keyup', { key: 's' });
+        cy.get('.viewport-canvas').trigger('keydown', { key: 'd' });
+        cy.wait(200);
+        cy.get('.viewport-canvas').trigger('keyup', { key: 'd' });
+
+        // Zooming
+        cy.get('canvas').trigger('wheel', { deltaY: -1000 });
+        cy.wait(300);
+        cy.get('canvas').trigger('wheel', { deltaY: 1000 });
+        cy.wait(300);
+        cy.get('canvas').trigger('wheel', { deltaY: -1000 });
+        cy.wait(300);
+
     });
+
 })
 
 // - /new/question, /question/:qid, commentSection, 
