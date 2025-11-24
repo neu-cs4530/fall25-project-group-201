9a8f095b72dea10b1571101836c7737f4ee1ed24
meredithscott131 <scott.mer@northeastern.edu>
2025-11-24 14:20:47 -0500

    Updated gallery form css

--- client/src/components/main/galleries/NewGalleryPostPage/index.tsx
+++ client/src/components/main/galleries/NewGalleryPostPage/index.tsx
@@ -234,7 +234,7 @@ const NewGalleryPostPage = () => {
 
           {form.mediaPath?.endsWith('.glb') && (
             <>
-              <h3>Add Thumbnail</h3>
+              <h2>Add Thumbnail</h2>
               <div className='file-upload'>
                 <input type='file' accept='image/*' onChange={handleThumbnailFileUpload} />
               </div>
