99cbb0945677e9f74f08b6e8e758cd0fa0260173
Erika Ding <erikajding@gmail.com>
2025-11-23 23:05:16 -0500

    Lint fixes

--- client/src/components/main/threeViewport/InfoPopover/index.tsx
+++ client/src/components/main/threeViewport/InfoPopover/index.tsx
@@ -11,14 +11,17 @@ const InfoPopover = () => {
       className='popover-content'
       role='dialog'
       aria-modal='true'>
-      Welcome to the 3D viewport!<br/>
-      &bull; Click and drag to turn <br/>
-      &bull; Scroll to zoom <br/>
-      &bull; Arrow keys to pan <br/>
-      &bull; WASD to tilt <br/>
-      &bull; Buttons (left to right): reset camera,<br/>
-      perspective/orthographic mode,<br/>
-      and HDRI lighting setups toggles <br/>
+      Welcome to the 3D viewport!
+      <br />
+      &bull; Click and drag to turn <br />
+      &bull; Scroll to zoom <br />
+      &bull; Arrow keys to pan <br />
+      &bull; WASD to tilt <br />
+      &bull; Buttons (left to right): reset camera,
+      <br />
+      perspective/orthographic mode,
+      <br />
+      and HDRI lighting setups toggles <br />
     </div>
   );
 };
