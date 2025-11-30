37f90bfcaeb8299a8bb827d3f0a7cc3625760ee5
Erika Ding <erikajding@gmail.com>
2025-11-30 14:47:01 -0500

    Added tests for testing markdown support

---
+++ client/tests/__mocks__/react-markdown.tsx
@@ -0,0 +1,5 @@
+const Markdown = ({ children }: { children: string }) => {
+  return <div data-testid="markdown-content">{children}</div>;
+};
+
+export default Markdown;
---
+++ client/tests/setupTests.ts
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom';
+
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/tests/setupTests.ts'],
+  testMatch: ['<rootDir>/tests/**/*.test.{ts,tsx}'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
